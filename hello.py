a81c625f8af2e5ca6f9e0cd475390a850d328aa0
AnteMulari <75026168+AnteMulari@users.noreply.github.com>
2020-11-25 15:30:34 +0200

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,9 @@
+### Do something - what - print
+### Data source? - what data is being printed?
+### Output device? - where the data is being printed?
+
+### I think these are rather good questions, it would be cool 
+### to specify these in the code
+
+
+print("Hello, World!\nI'm Ante")
